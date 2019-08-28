a4fc23feb6ca96feefafe8e306d171a4b4a58c2e
artision <art.2413hotmail.com>
2019-08-28 18:30:13 +0200

    second commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { ruta } from './app.router';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    ServicesComponent,
+    HomeComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
